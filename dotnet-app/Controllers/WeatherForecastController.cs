93f0b4c48ff85f0dbb5eaab527952457588b8d89
Gyan Chand <gyanchand.mohanty@apmosys.com>
2026-01-19 23:48:47 +0530

    app version-4

--- dotnet-app/Controllers/WeatherForecastController.cs
+++ dotnet-app/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace DotnetApp.Controllers
         [HttpGet]
         public string Get()
         {
-            return "Hello from .NET App v3";
+            return "Hello from .NET App v4";
         }
     }
 }
